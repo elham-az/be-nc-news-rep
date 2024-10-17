4d8c857f9b696c67818fd46ce4f5616854d2d580
elham <elhamazizi05@gmail.com>
2024-10-17 13:49:56 +0100

    ticket-4 get articles by id

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 const {getAllTopicsController} = require('./controllers/topics-controller')
 const {getEndpointsController} = require('./controllers/endpoints-controller')
+const {getArticlesController} = require('./controllers/articles-controller')
 
 app.use(express.json())
 
 app.get('/api/topics', getAllTopicsController)
 
 app.get('/api', getEndpointsController)
+
+app.get('/api/articles/:article_id', getArticlesController)
+
+app.use((err, req, res, next) => {
+    if (err.status) {
+        res.status(err.status).send({ msg: err.msg })
+    } else {
+        res.status(500).send({ msg: 'Internal Server Error' })
+    }
+})
 
 // app.get('/api', (req, res) => {
 //   const filePath = `${__dirname}/endpoints.json`
---
+++ controllers/articles-controller.js
@@ -0,0 +1,18 @@
+const { getArticles } = require('../models/articles-model')
+
+const getArticlesController = (req, res, next) => {
+    const { article_id } = req.params
+    console.log(article_id)
+    if (isNaN(Number(article_id))) {
+        return res.status(400).send({msg: 'Invalid article id'})
+    }
+    getArticles(article_id).then((article)=> {
+        
+        res.status(200).send({article})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+
+module.exports = { getArticlesController }
